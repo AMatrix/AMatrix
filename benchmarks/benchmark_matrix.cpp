bfe950c20837fa0bc07be83c62e72b1ff15082b6
pooyan-dadvand <pooyan@cimne.upc.edu>
2017-10-31 18:33:51 +0100

    Apply styling

--- benchmarks/benchmark_matrix.cpp
+++ benchmarks/benchmark_matrix.cpp
@@ -5,93 +5,87 @@
 //#include "Eigen/Dense"
 
 class Timer {
-	std::chrono::high_resolution_clock::time_point _start;
-public:
-	Timer() : _start(std::chrono::high_resolution_clock::now()) {
-	}
-	std::chrono::milliseconds elapsed() {
-		return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - _start);
-	}
-	void reset() {
-		_start = std::chrono::high_resolution_clock::now();
-	}
+    std::chrono::high_resolution_clock::time_point _start;
+
+   public:
+    Timer() : _start(std::chrono::high_resolution_clock::now()) {}
+    std::chrono::milliseconds elapsed() {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::high_resolution_clock::now() - _start);
+    }
+    void reset() { _start = std::chrono::high_resolution_clock::now(); }
 };
 
-template<typename TMatrixType>
+template <typename TMatrixType>
 long long MeasureAccessTime(TMatrixType& A) {
-	int repeat_number = 1000000;
-	Timer timer;
-	for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++)
-		for (int i = 0; i < 3; i++)
-			for (int j = 0; j < 3; j++)
-				A(i, j) += i - j;
-	return timer.elapsed().count();
+    int repeat_number = 1000000;
+    Timer timer;
+    for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++)
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                A(i, j) += i - j;
+    return timer.elapsed().count();
 }
 
-
-template<typename TMatrixType>
+template <typename TMatrixType>
 std::size_t MeasureAssignTime(TMatrixType& A) {
-	int repeat_number = 1000000;
-	TMatrixType B = A;
-	Timer timer;
-	for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++) {
-		for (int i = 0; i < 3; i++)
-			for (int j = 0; j < 3; j++)
-				B(i, j) += i - j;
-		A = B;
-	}
+    int repeat_number = 1000000;
+    TMatrixType B = A;
+    Timer timer;
+    for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++) {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                B(i, j) += i - j;
+        A = B;
+    }
 
-	return timer.elapsed().count();
+    return timer.elapsed().count();
 }
 
-template<typename TMatrixType>
+template <typename TMatrixType>
 std::size_t MeasureSumTime(TMatrixType& A, TMatrixType& B) {
-	int repeat_number = 10000;
-	TMatrixType C = A;
-	TMatrixType D = A;
-	Timer timer;
-	for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++) {
-		C = A + B ; 
-		B = C;
-	}
+    int repeat_number = 10000;
+    TMatrixType C = A;
+    TMatrixType D = A;
+    Timer timer;
+    for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++) {
+        C = A + B;
+        B = C;
+    }
 
-	return timer.elapsed().count();
+    return timer.elapsed().count();
 }
 
-template<typename TMatrixType1, typename TMatrixType2>
+template <typename TMatrixType1, typename TMatrixType2>
 std::size_t MeasureMultTime(TMatrixType1& A, TMatrixType2& B) {
-	int repeat_number = 1000;
-	TMatrixType1 C = A;
-	TMatrixType1 D = A;
-	Timer timer;
-	for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++) {
-		D = A;
-		for (int i = 0; i < 1000; i++) {
-			C = D * B;
-			D = C;
-		}
-	}
+    int repeat_number = 1000;
+    TMatrixType1 C = A;
+    TMatrixType1 D = A;
+    Timer timer;
+    for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++) {
+        D = A;
+        for (int i = 0; i < 1000; i++) {
+            C = D * B;
+            D = C;
+        }
+    }
 
-	return timer.elapsed().count();
+    return timer.elapsed().count();
 }
 
-void CompareAccessTime(){
-	AMatrix::Matrix<double,3,3> m_33(0.00);
-	std::cout << "A[i,j] += i -j\t\t" << MeasureAccessTime(m_33) << std::endl;
+void CompareAccessTime() {
+    AMatrix::Matrix<double, 3, 3> m_33(0.00);
+    std::cout << "A[i,j] += i -j\t\t" << MeasureAccessTime(m_33) << std::endl;
 }
 
-void CompareAssignTime(){
-	AMatrix::Matrix<double,3,3> m_33(0.00);
-	std::cout << "A = B\t\t\t" << MeasureAssignTime(m_33) << std::endl;
+void CompareAssignTime() {
+    AMatrix::Matrix<double, 3, 3> m_33(0.00);
+    std::cout << "A = B\t\t\t" << MeasureAssignTime(m_33) << std::endl;
 }
 
-
-
-int main()
-{
-	std::cout << "operation\t\tAMatrix\t\tEigen\t\tUblas\t\tAtlas" << std::endl;
-	CompareAccessTime();
-	CompareAssignTime();
-	return 0;
+int main() {
+    std::cout << "operation\t\tAMatrix\t\tEigen\t\tUblas\t\tAtlas" << std::endl;
+    CompareAccessTime();
+    CompareAssignTime();
+    return 0;
 }
-
