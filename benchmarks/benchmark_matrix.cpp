ad6189e16dbe508331abce421669b80c29a69b02
pooyan-dadvand <pooyan@cimne.upc.edu>
2017-11-06 10:54:31 +0100

    removing assign and access comparisons

--- benchmarks/benchmark_matrix.cpp
+++ benchmarks/benchmark_matrix.cpp
@@ -11,90 +11,71 @@
 template <typename TMatrixType, int NumberOfRows, int NumberOfColumns>
 class ComparisonColumn {
     TMatrixType mA;
-    void initialize(TMatrixType& TheMatrix) {
+    TMatrixType mB;
+    void initialize(TMatrixType& TheMatrix, double Value) {
         for (int i = 0; i < NumberOfRows; i++)
             for (int j = 0; j < NumberOfColumns; j++)
-                TheMatrix(i, j) = 0.00;
+                TheMatrix(i, j) = Value;
     }
 
    public:
-    ComparisonColumn() { initialize(mA); }
+    ComparisonColumn() {
+        initialize(mA, 0.01);
+        initialize(mB, 1.00);
+    }
 
-    Timer::duration_type MeasureAccessTime() {
+    Timer::duration_type MeasureSumTime() {
         int repeat_number = 10000000;
+        TMatrixType C = mA;
         Timer timer;
-        for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++)
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
-                    mA(i, j) += i - j;
+        for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++) {
+            C = mA + mB;
+            mB = C;
+        }
+
+        return timer.elapsed().count();
+    }
+
+    Timer::duration_type MeasureMultTime() {
+        int repeat_number = 100000000;
+        TMatrixType C = mA;
+        TMatrixType D = mA;
+        Timer timer;
+        for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++) {
+            D = mA;
+            for (int i = 0; i < 1000; i++) {
+                C = D * mB;
+                D = C;
+            }
+        }
+
         return timer.elapsed().count();
     }
 };
 
-template <typename TMatrixType>
-Timer::duration_type MeasureAssignTime(TMatrixType& A) {
-    int repeat_number = 10000000;
-    TMatrixType B = A;
-    Timer timer;
-    for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++) {
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                B(i, j) += i - j;
-        A = B;
-    }
-
-    return timer.elapsed().count();
-}
-
-template <typename TMatrixType>
-Timer::duration_type MeasureSumTime(TMatrixType& A, TMatrixType& B) {
-    int repeat_number = 10000;
-    TMatrixType C = A;
-    TMatrixType D = A;
-    Timer timer;
-    for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++) {
-        C = A + B;
-        B = C;
-    }
-
-    return timer.elapsed().count();
-}
-
-template <typename TMatrixType1, typename TMatrixType2>
-Timer::duration_type MeasureMultTime(TMatrixType1& A, TMatrixType2& B) {
-    int repeat_number = 1000;
-    TMatrixType1 C = A;
-    TMatrixType1 D = A;
-    Timer timer;
-    for (int i_repeat = 0; i_repeat < repeat_number; i_repeat++) {
-        D = A;
-        for (int i = 0; i < 1000; i++) {
-            C = D * B;
-            D = C;
-        }
-    }
-
-    return timer.elapsed().count();
-}
-
-void CompareAccessTime() {
+void CompareSumTime() {
     ComparisonColumn<AMatrix::Matrix<double, 3, 3>, 3, 3> a_matrix_column;
-    std::cout << "A[i,j] += i -j\t\t" << a_matrix_column.MeasureAccessTime();
+    std::cout << "C = A + B\t\t" << a_matrix_column.MeasureSumTime();
 #if defined(AMATRIX_COMPARE_WITH_EIGEN)
     ComparisonColumn<Eigen::Matrix<double, 3, 3>, 3, 3> eigen_column;
-    std::cout << "\t\t" << eigen_column.MeasureAccessTime();
+    std::cout << "t\t" << eigen_column.MeasureSumTime();
 #endif
     std::cout << std::endl;
 }
 
-void CompareAssignTime() {
-    AMatrix::Matrix<double, 3, 3> m_33(0.00);
-    std::cout << "A = B\t\t\t" << MeasureAssignTime(m_33) << std::endl;
+void CompareMultTime() {
+    ComparisonColumn<AMatrix::Matrix<double, 3, 3>, 3, 3> a_matrix_column;
+    std::cout << "C = A * B\t\t" << a_matrix_column.MeasureMultTime();
+#if defined(AMATRIX_COMPARE_WITH_EIGEN)
+    ComparisonColumn<Eigen::Matrix<double, 3, 3>, 3, 3> eigen_column;
+    std::cout << "\t\t" << eigen_column.MeasureMultTime();
+#endif
+    std::cout << std::endl;
 }
 
 int main() {
     std::cout << "operation\t\tAMatrix\t\tEigen\t\tUblas\t\tAtlas" << std::endl;
-    CompareAccessTime();
-    CompareAssignTime();
+    CompareSumTime();
+    CompareMultTime();
     return 0;
 }
