0e9dcf760467c286df95a25603c48df2004916a2
pooyan-dadvand <pooyan@cimne.upc.edu>
2017-11-15 14:44:06 +0100

    Add almost igual macro

--- test/checks.h
+++ test/checks.h
@@ -3,3 +3,9 @@
         std::cout << a << " is not equal to " << b << std::endl; \
         return 1;                                                \
     }
+
+#define AMATRIX_CHECK_ALMOST_EQUAL(a, b)                         \
+    if (std::abs(a - b) > std::numeric_limit<double>::epsilon) { \
+        std::cout << a << " is not equal to " << b << std::endl; \
+        return 1;                                                \
+    }
