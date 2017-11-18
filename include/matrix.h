7f8ee9dca7345f1c57e4b9ad33932d3b9bedbd8e
pooyan-dadvand <pooyan@cimne.upc.edu>
2017-11-18 08:33:26 +0100

    Specializing plus and minus operators for dynamic matrix

--- include/matrix.h
+++ include/matrix.h
@@ -342,6 +342,30 @@ Matrix<TDataType, TSize1, TSize2> operator-(
     return result;
 }
 
+template <typename TDataType>
+Matrix<TDataType, 0, 0> operator+(
+    Matrix<TDataType, 0, 0> const& First,
+    Matrix<TDataType, 0, 0> const& Second) {
+    Matrix<TDataType, 0, 0> result(First.size1(), First.size2());
+    for (std::size_t i = 0; i < First.size1(); i++)
+        for (std::size_t j = 0; j < First.size2(); j++)
+            result(i, j) = First(i, j) + Second(i, j);
+
+    return result;
+}
+
+template <typename TDataType>
+Matrix<TDataType, 0, 0> operator-(
+    Matrix<TDataType, 0, 0> const& First,
+    Matrix<TDataType, 0, 0> const& Second) {
+    Matrix<TDataType, 0, 0> result(First.size1(), First.size2());
+    for (std::size_t i = 0; i < First.size1(); i++)
+        for (std::size_t j = 0; j < First.size2(); j++)
+            result(i, j) = First(i, j) - Second(i, j);
+
+    return result;
+}
+
 template <typename TDataType, std::size_t TSize1, std::size_t TSize2>
 bool operator!=(Matrix<TDataType, TSize1, TSize2> const& First,
     Matrix<TDataType, TSize1, TSize2> const& Second) {
