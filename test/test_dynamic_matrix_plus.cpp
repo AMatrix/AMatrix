2d5a9b1f5ef4e0c7e155166e4cca479748ac93f0
pooyan-dadvand <pooyan@cimne.upc.edu>
2017-11-18 18:15:09 +0100

    Adding tests for dynamic matrix plus and minus

---
+++ test/test_dynamic_matrix_plus.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+#include "matrix.h"
+#include "checks.h"
+
+std::size_t TestMatrixPlus(std::size_t Size1, std::size_t Size2) {
+    AMatrix::Matrix<double, 0, 0> a_matrix(Size1, Size2);
+    AMatrix::Matrix<double, 0, 0> b_matrix(Size1, Size2);
+    AMatrix::Matrix<double, 0, 0> c_matrix(Size1, Size2);
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            a_matrix(i, j) = 2.33 * i - 4.52 * j;
+
+    b_matrix = a_matrix;
+    c_matrix = a_matrix + b_matrix;
+
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            AMATRIX_CHECK_EQUAL(c_matrix(i, j), 2 * (2.33 * i - 4.52 * j));
+
+    return 0;  // not failed
+}
+
+std::size_t TestMatrixPlusEqual(std::size_t Size1, std::size_t Size2) {
+    AMatrix::Matrix<double, 0, 0> a_matrix(Size1, Size2);
+    AMatrix::Matrix<double, 0, 0> b_matrix(Size1, Size2);
+    AMatrix::Matrix<double, 0, 0> c_matrix(Size1, Size2);
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            a_matrix(i, j) = 2.33 * i - 4.52 * j;
+
+    b_matrix = a_matrix;
+    a_matrix += b_matrix;
+
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            AMATRIX_CHECK_EQUAL(a_matrix(i, j), 2 * (2.33 * i - 4.52 * j));
+
+    return 0;  // not failed
+}
+
+int main() {
+    std::size_t number_of_failed_tests = 0;
+	number_of_failed_tests += TestMatrixPlus(1,1);
+
+	number_of_failed_tests += TestMatrixPlus(1,2);
+	number_of_failed_tests += TestMatrixPlus(2,1);
+	number_of_failed_tests += TestMatrixPlus(2,2);
+
+	number_of_failed_tests += TestMatrixPlus(3,1);
+	number_of_failed_tests += TestMatrixPlus(3,2);
+	number_of_failed_tests += TestMatrixPlus(3,3);
+	number_of_failed_tests += TestMatrixPlus(1,3);
+	number_of_failed_tests += TestMatrixPlus(2,3);
+	number_of_failed_tests += TestMatrixPlus(3,3);
+
+ 	number_of_failed_tests += TestMatrixPlusEqual(1,1);
+
+	number_of_failed_tests += TestMatrixPlusEqual(1,2);
+	number_of_failed_tests += TestMatrixPlusEqual(2,1);
+	number_of_failed_tests += TestMatrixPlusEqual(2,2);
+
+	number_of_failed_tests += TestMatrixPlusEqual(3,1);
+	number_of_failed_tests += TestMatrixPlusEqual(3,2);
+	number_of_failed_tests += TestMatrixPlusEqual(3,3);
+	number_of_failed_tests += TestMatrixPlusEqual(1,3);
+	number_of_failed_tests += TestMatrixPlusEqual(2,3);
+	number_of_failed_tests += TestMatrixPlusEqual(3,3);
+
+    std::cout << number_of_failed_tests << "tests failed" << std::endl;
+
+    return number_of_failed_tests;
+}
---
+++ test/test_dynamic_matrix_product.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+#include "matrix.h"
+#include "checks.h"
+
+template <std::size_t NumberOfRows, std::size_t NumberOfCulomns>
+std::size_t TestMatrixScalarProduct() {
+    AMatrix::Matrix<double, 0, 0> a_matrix(NumberOfRows, NumberOfCulomns);
+    AMatrix::Matrix<double, 0, 0> b_matrix(NumberOfRows, NumberOfCulomns);
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            a_matrix(i, j) = 2.33 * i - 4.52 * j;
+
+    b_matrix = 1.3 * a_matrix * 0.34;
+    b_matrix = 2 * b_matrix;
+
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            AMATRIX_CHECK_EQUAL(b_matrix(i, j), 2 * 1.3 * (2.33 * i - 4.52 * j) * 0.34);
+
+    return 0;  // not failed
+}
+
+template <std::size_t NumberOfRows, std::size_t NumberOfCulomns,
+    std::size_t NumberOfSecondRows>
+std::size_t TestMatrixProduct() {
+    std::cout << "Testing A(" << NumberOfRows << "," << NumberOfCulomns
+              << ") X B(" << NumberOfCulomns << "," << NumberOfSecondRows
+              << ") ";
+    AMatrix::Matrix<double, 0, 0> a_matrix(NumberOfRows, NumberOfCulomns);
+    AMatrix::Matrix<double, 0, 0> b_matrix(NumberOfCulomns, NumberOfSecondRows);
+    AMatrix::Matrix<double, 0, 0> c_matrix(NumberOfRows, NumberOfSecondRows);
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            if (i == j)
+                a_matrix(i, j) = 2.33 * (i + 1);
+            else
+                a_matrix(i, j) = 0.00;
+
+    for (std::size_t i = 0; i < b_matrix.size1(); i++)
+        for (std::size_t j = 0; j < b_matrix.size2(); j++)
+            b_matrix(i, j) = i + j + 1;
+
+    c_matrix = a_matrix * b_matrix;
+
+    for (std::size_t i = 0; i < c_matrix.size1(); i++)
+        for (std::size_t j = 0; j < c_matrix.size2(); j++)
+            if (static_cast<std::size_t>(i) < NumberOfCulomns)
+                AMATRIX_CHECK_EQUAL(
+                    c_matrix(i, j), b_matrix(i, j) * (i + 1) * 2.33);
+
+    std::cout << "OK" << std::endl;
+    return 0;  // not failed
+}
+
+int main() {
+    std::size_t number_of_failed_tests = 0;
+
+    // scalar product test
+    number_of_failed_tests += TestMatrixScalarProduct<1, 1>();
+
+    number_of_failed_tests += TestMatrixScalarProduct<1, 2>();
+    number_of_failed_tests += TestMatrixScalarProduct<2, 1>();
+    number_of_failed_tests += TestMatrixScalarProduct<2, 2>();
+
+    number_of_failed_tests += TestMatrixScalarProduct<3, 1>();
+    number_of_failed_tests += TestMatrixScalarProduct<3, 2>();
+    number_of_failed_tests += TestMatrixScalarProduct<3, 3>();
+    number_of_failed_tests += TestMatrixScalarProduct<1, 3>();
+    number_of_failed_tests += TestMatrixScalarProduct<2, 3>();
+    number_of_failed_tests += TestMatrixScalarProduct<3, 3>();
+
+    // matrix product test
+    number_of_failed_tests += TestMatrixProduct<1, 1, 1>();
+
+    number_of_failed_tests += TestMatrixProduct<1, 1, 2>();
+    number_of_failed_tests += TestMatrixProduct<2, 1, 1>();
+    number_of_failed_tests += TestMatrixProduct<2, 1, 2>();
+
+    number_of_failed_tests += TestMatrixProduct<3, 1, 1>();
+    number_of_failed_tests += TestMatrixProduct<3, 1, 2>();
+    number_of_failed_tests += TestMatrixProduct<3, 1, 3>();
+    number_of_failed_tests += TestMatrixProduct<1, 1, 3>();
+    number_of_failed_tests += TestMatrixProduct<2, 1, 3>();
+    number_of_failed_tests += TestMatrixProduct<3, 1, 3>();
+
+    number_of_failed_tests += TestMatrixProduct<1, 2, 1>();
+
+    number_of_failed_tests += TestMatrixProduct<1, 2, 2>();
+    number_of_failed_tests += TestMatrixProduct<2, 2, 1>();
+    number_of_failed_tests += TestMatrixProduct<2, 2, 2>();
+
+    number_of_failed_tests += TestMatrixProduct<3, 2, 1>();
+    number_of_failed_tests += TestMatrixProduct<3, 2, 2>();
+    number_of_failed_tests += TestMatrixProduct<3, 2, 3>();
+    number_of_failed_tests += TestMatrixProduct<1, 2, 3>();
+    number_of_failed_tests += TestMatrixProduct<2, 2, 3>();
+    number_of_failed_tests += TestMatrixProduct<3, 2, 3>();
+
+    number_of_failed_tests += TestMatrixProduct<1, 3, 1>();
+
+    number_of_failed_tests += TestMatrixProduct<1, 3, 2>();
+    number_of_failed_tests += TestMatrixProduct<2, 3, 1>();
+    number_of_failed_tests += TestMatrixProduct<2, 3, 2>();
+
+    number_of_failed_tests += TestMatrixProduct<3, 3, 1>();
+    number_of_failed_tests += TestMatrixProduct<3, 3, 2>();
+    number_of_failed_tests += TestMatrixProduct<3, 3, 3>();
+    number_of_failed_tests += TestMatrixProduct<1, 3, 3>();
+    number_of_failed_tests += TestMatrixProduct<2, 3, 3>();
+    number_of_failed_tests += TestMatrixProduct<3, 3, 3>();
+
+    std::cout << number_of_failed_tests << " tests failed" << std::endl;
+
+    return number_of_failed_tests;
+}
