887d96c68cfad8a0dd530145055576fa5cf40708
pooyan-dadvand <pooyan@cimne.upc.edu>
2017-11-29 08:51:01 +0100

    Moving transpose matrix to matrix_expression.h

--- include/matrix_expression.h
+++ include/matrix_expression.h
@@ -81,4 +81,50 @@ class MatrixExpression {
     //     return TransposeMatrix<TDataType, TSize1, TSize2>(*this);
     // }
 };
+
+
+template <typename TDataType, std::size_t TSize1, std::size_t TSize2>
+class Matrix;
+template <typename TDataType, std::size_t TSize1, std::size_t TSize2>
+class TransposeMatrix : public Matrix<TDataType, TSize1, TSize2> {
+    Matrix<TDataType, TSize1, TSize2> const& mOriginal;
+
+   public:
+    using value_type = TDataType;
+    TransposeMatrix() = delete;
+
+    TransposeMatrix(Matrix<TDataType, TSize1, TSize2> const& Original)
+        : mOriginal(Original) {}
+
+    inline value_type const& operator()(std::size_t i, std::size_t j) const {
+        return mOriginal(j, i);
+    }
+
+    inline static constexpr std::size_t size1() {
+        return Matrix<TDataType, TSize1, TSize2>::size2();
+    }
+    inline static constexpr std::size_t size2() {
+        return Matrix<TDataType, TSize1, TSize2>::size1();
+    }
+};
+
+template <typename TDataType>
+class TransposeMatrix<TDataType, 0, 0> {
+    Matrix<TDataType, 0, 0> const& mOriginal;
+
+   public:
+    using value_type = TDataType;
+    TransposeMatrix() = delete;
+
+    TransposeMatrix(Matrix<TDataType, 0, 0> const& Original)
+        : mOriginal(Original) {}
+
+    inline value_type const& operator()(std::size_t i, std::size_t j) const {
+        return mOriginal(j, i);
+    }
+
+    inline std::size_t size1() { return mOriginal.size2(); }
+    inline std::size_t size2() { return mOriginal.size1(); }
+};
+
 }  // namespace AMatrix
