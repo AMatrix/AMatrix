a53f2cb27ff67ae9195f717227aeaaa3ec1724ea
pooyan-dadvand <pooyan@cimne.upc.edu>
2017-12-10 21:52:03 +0100

    Adding sum expression template

--- include/matrix_expression.h
+++ include/matrix_expression.h
@@ -61,12 +61,44 @@ class ZeroMatrix : public MatrixExpression<ZeroMatrix<TDataType>> {
     ZeroMatrix(std::size_t Size1, std::size_t Size2)
         : _size1(Size1), _size2(Size2) {}
 
-    inline TDataType const operator()(std::size_t i, std::size_t j) const {
+    inline TDataType const& operator()(std::size_t i, std::size_t j) const {
         return TDataType();
     }
 
     inline std::size_t size1() const { return _size1; }
     inline std::size_t size2() const { return _size2; }
 };
+
+
+template <typename TExpression1Type, typename TExpression2Type>
+class MatrixSumExpression
+    : public MatrixExpression<MatrixSumExpression<TExpression1Type, TExpression2Type>> {
+    TExpression1Type const& _first;
+    TExpression2Type const& _second;
+
+   public:
+    MatrixSumExpression(
+        TExpression1Type const& First, TExpression2Type const& Second)
+        : _first(First), _second(Second) {}
+    using data_type = typename TExpression1Type::data_type;
+
+    std::size_t size1() const { return _first.size1(); }
+
+    std::size_t size2() const { return _first.size2(); }
+
+    std::size_t size() const { return _first.size(); }
+
+    inline data_type operator()(std::size_t i, std::size_t j) const {
+        return _first(i, j) + _second(i, j);
+    }
+};
+
+template <typename TFirstMatrixType, typename TSecondMatrixType>
+MatrixSumExpression<TFirstMatrixType, TSecondMatrixType> operator+(
+    TFirstMatrixType const& First, TSecondMatrixType const& Second) {
+    return MatrixSumExpression<TFirstMatrixType, TSecondMatrixType>(
+        First, Second);
+}
+
 
 }  // namespace AMatrix
