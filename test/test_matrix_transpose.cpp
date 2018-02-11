21b4e25b161270003fe52ed8cc3aac9f83afa3ff
pooyan-dadvand <pooyan@cimne.upc.edu>
2018-02-11 17:44:29 +0100

    Adding trnaspose product test

--- test/test_matrix_transpose.cpp
+++ test/test_matrix_transpose.cpp
@@ -19,6 +19,38 @@ std::size_t TestMatrixTranspose() {
 }
 
 
+template <std::size_t NumberOfRows, std::size_t NumberOfCulomns,
+    std::size_t NumberOfSecondRows>
+std::size_t TestMatrixTransposeProduct() {
+    std::cout << "Testing A(" << NumberOfRows << "," << NumberOfCulomns
+              << ") X B(" << NumberOfCulomns << "," << NumberOfSecondRows
+              << ") ";
+    AMatrix::Matrix<double, NumberOfRows, NumberOfCulomns> a_matrix;
+    AMatrix::Matrix<double, NumberOfCulomns, NumberOfSecondRows> b_matrix;
+    AMatrix::Matrix<double, NumberOfRows, NumberOfSecondRows> c_matrix;
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            if (i == j)
+                a_matrix(i, j) = 2.33 * (i + 1);
+            else
+                a_matrix(i, j) = 0.00;
+
+    for (std::size_t i = 0; i < b_matrix.size1(); i++)
+        for (std::size_t j = 0; j < b_matrix.size2(); j++)
+            b_matrix(i, j) = i + j + 1;
+
+    c_matrix = a_matrix * b_matrix.transpose();
+
+    for (std::size_t i = 0; i < c_matrix.size1(); i++)
+        for (std::size_t j = 0; j < c_matrix.size2(); j++)
+            if (static_cast<std::size_t>(i) < NumberOfCulomns)
+                AMATRIX_CHECK_EQUAL(
+                    c_matrix(i, j), b_matrix(j, i) * (i + 1) * 2.33);
+
+    std::cout << "OK" << std::endl;
+    return 0;  // not failed
+}
+
 int main()
 {
 	std::size_t number_of_failed_tests = 0;
@@ -34,6 +66,46 @@ int main()
 	number_of_failed_tests += TestMatrixTranspose<1,3>();
 	number_of_failed_tests += TestMatrixTranspose<2,3>();
 	number_of_failed_tests += TestMatrixTranspose<3,3>();
+
+    // matrix transpose product test
+    number_of_failed_tests += TestMatrixTransposeProduct<1, 1, 1>();
+
+    number_of_failed_tests += TestMatrixTransposeProduct<1, 1, 2>();
+    number_of_failed_tests += TestMatrixTransposeProduct<2, 1, 1>();
+    number_of_failed_tests += TestMatrixTransposeProduct<2, 1, 2>();
+
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 1, 1>();
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 1, 2>();
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 1, 3>();
+    number_of_failed_tests += TestMatrixTransposeProduct<1, 1, 3>();
+    number_of_failed_tests += TestMatrixTransposeProduct<2, 1, 3>();
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 1, 3>();
+
+    number_of_failed_tests += TestMatrixTransposeProduct<1, 2, 1>();
+
+    number_of_failed_tests += TestMatrixTransposeProduct<1, 2, 2>();
+    number_of_failed_tests += TestMatrixTransposeProduct<2, 2, 1>();
+    number_of_failed_tests += TestMatrixTransposeProduct<2, 2, 2>();
+
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 2, 1>();
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 2, 2>();
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 2, 3>();
+    number_of_failed_tests += TestMatrixTransposeProduct<1, 2, 3>();
+    number_of_failed_tests += TestMatrixTransposeProduct<2, 2, 3>();
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 2, 3>();
+
+    number_of_failed_tests += TestMatrixTransposeProduct<1, 3, 1>();
+
+    number_of_failed_tests += TestMatrixTransposeProduct<1, 3, 2>();
+    number_of_failed_tests += TestMatrixTransposeProduct<2, 3, 1>();
+    number_of_failed_tests += TestMatrixTransposeProduct<2, 3, 2>();
+
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 3, 1>();
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 3, 2>();
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 3, 3>();
+    number_of_failed_tests += TestMatrixTransposeProduct<1, 3, 3>();
+    number_of_failed_tests += TestMatrixTransposeProduct<2, 3, 3>();
+    number_of_failed_tests += TestMatrixTransposeProduct<3, 3, 3>();
 
 	std::cout << number_of_failed_tests << "tests failed" << std::endl;
 
