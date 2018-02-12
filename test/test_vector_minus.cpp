cdb51089d031104a08dab5a42a1c27bda7f30656
pooyan-dadvand <pooyan@cimne.upc.edu>
2018-02-12 17:13:25 +0100

    Adding vector minus test

---
+++ test/test_vector_minus.cpp
@@ -0,0 +1,50 @@
+#include "matrix.h"
+#include "checks.h"
+
+template <std::size_t TSize>
+std::size_t TestVectorMinus() {
+    AMatrix::Vector<double, TSize> a_vector;
+    AMatrix::Vector<double, TSize> b_vector;
+    AMatrix::Vector<double, TSize> c_vector;
+    for (std::size_t i = 0; i < a_vector.size(); i++)
+            a_vector[i] = 2.33 * i - 4.52;
+
+    b_vector = a_vector;
+    c_vector = a_vector - b_vector;
+
+    for (std::size_t i = 0; i < a_vector.size(); i++)
+            AMATRIX_CHECK_EQUAL(c_vector[i], 0.00);
+
+    return 0;  // not failed
+}
+
+template <std::size_t TSize>
+std::size_t TestMatrixMinusEqual() {
+    AMatrix::Vector<double, TSize> a_vector;
+    AMatrix::Vector<double, TSize> b_vector;
+    for (std::size_t i = 0; i < a_vector.size(); i++)
+            a_vector[i] = 2.33 * i - 4.52;
+
+    b_vector = a_vector;
+    a_vector -= b_vector;
+
+    for (std::size_t i = 0; i < a_vector.size(); i++)
+            AMATRIX_CHECK_EQUAL(a_vector[i], 0.00);
+
+    return 0;  // not failed
+}
+
+int main() {
+    std::size_t number_of_failed_tests = 0;
+    number_of_failed_tests += TestVectorMinus<1>();
+    number_of_failed_tests += TestVectorMinus<2>();
+    number_of_failed_tests += TestVectorMinus<3>();
+
+    number_of_failed_tests += TestMatrixMinusEqual<1>();
+    number_of_failed_tests += TestMatrixMinusEqual<2>();
+    number_of_failed_tests += TestMatrixMinusEqual<3>();
+
+    std::cout << number_of_failed_tests << "tests failed" << std::endl;
+
+    return number_of_failed_tests;
+}
