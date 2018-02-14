ec9ffbda813e7a8ef31313d4ffddd4e43746a708
pooyan-dadvand <pooyan@cimne.upc.edu>
2018-02-14 09:13:47 +0100

    Adding test for norm

---
+++ test/test_vector_norm.cpp
@@ -0,0 +1,31 @@
+#include "matrix.h"
+#include "checks.h"
+
+template <std::size_t TSize>
+std::size_t TestVectorNorm() {
+    AMatrix::Vector<double, TSize> a_vector;
+    AMatrix::Vector<double, TSize> b_vector;
+    double results[] = {0,1,5,14,30,55,91,140,204,285};
+
+    for (std::size_t i = 0; i < a_vector.size(); i++)
+        a_vector[i] = i+1;
+
+    AMATRIX_CHECK_EQUAL(a_vector.norm(), results[TSize]);
+
+    std::cout << "OK" << std::endl;
+    return 0;  // not failed
+}
+
+int main() {
+    std::size_t number_of_failed_tests = 0;
+
+    // norm test
+    number_of_failed_tests += TestVectorNorm<1>();
+    number_of_failed_tests += TestVectorNorm<2>();
+    number_of_failed_tests += TestVectorNorm<3>();
+ 
+
+    std::cout << number_of_failed_tests << " tests failed" << std::endl;
+
+    return number_of_failed_tests;
+}
