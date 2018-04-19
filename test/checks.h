8f6a9a505798a67d8bc55c8bb63b867305e7ce3e
pooyan-dadvand <pooyan@cimne.upc.edu>
2018-04-19 13:23:40 +0200

    Adding check for true/false

--- test/checks.h
+++ test/checks.h
@@ -1,3 +1,9 @@
+#define AMATRIX_CHECK(a)                               \
+    if (!(a)) {                                          \
+        std::cout << #a << " is not true" << std::endl; \
+        return 1;                                      \
+    }
+
 #define AMATRIX_CHECK_EQUAL(a, b)                                \
     if (a != b) {                                                \
         std::cout << a << " is not equal to " << b << std::endl; \
