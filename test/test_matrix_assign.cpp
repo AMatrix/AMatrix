e4cae4dab60b6114b52b8a7cec200ccfaa79b4f5
pooyan-dadvand <pooyan@cimne.upc.edu>
2018-04-24 13:00:06 +0200

    Adding IdentityMatrix test

--- test/test_matrix_assign.cpp
+++ test/test_matrix_assign.cpp
@@ -1,70 +1,85 @@
 #include "amatrix.h"
 #include "checks.h"
 
-
-
-
-template<std::size_t TSize1, std::size_t TSize2>
+template <std::size_t TSize1, std::size_t TSize2>
 std::size_t TestZeroMatrixAssign() {
-	AMatrix::Matrix<double, TSize1,TSize2> a_matrix;
-	a_matrix = AMatrix::ZeroMatrix<double>(TSize1, TSize2);
-	
-	for (std::size_t i = 0; i < a_matrix.size1(); i++)
-		for (std::size_t j = 0; j < a_matrix.size2(); j++)
-			AMATRIX_CHECK_EQUAL(a_matrix(i,j), 0.00);
+    AMatrix::Matrix<double, TSize1, TSize2> a_matrix;
+    a_matrix = AMatrix::ZeroMatrix<double>(TSize1, TSize2);
 
-	return 0; // not failed
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            AMATRIX_CHECK_EQUAL(a_matrix(i, j), 0.00);
+
+    return 0;  // not failed
 }
 
-template<std::size_t TSize1, std::size_t TSize2>
+template <std::size_t TSize>
+std::size_t TestIdentityMatrixAssign() {
+    AMatrix::Matrix<double, TSize, TSize> a_matrix;
+    a_matrix = AMatrix::IdentityMatrix<double>(TSize);
+
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++){
+            if (i == j) {
+                AMATRIX_CHECK_EQUAL(a_matrix(i, j), 1.00);
+            } else {
+                AMATRIX_CHECK_EQUAL(a_matrix(i, j), 0.00);
+            }
+		}
+		
+    return 0;  // not failed
+}
+
+template <std::size_t TSize1, std::size_t TSize2>
 std::size_t TestMatrixAssign() {
-	AMatrix::Matrix<double, TSize1,TSize2> a_matrix;
-	AMatrix::Matrix<double, TSize1,TSize2> b_matrix;
-	for (std::size_t i = 0; i < a_matrix.size1(); i++)
-		for (std::size_t j = 0; j < a_matrix.size2(); j++)
-			a_matrix(i, j) =  2.33 * i - 4.52 * j;
+    AMatrix::Matrix<double, TSize1, TSize2> a_matrix;
+    AMatrix::Matrix<double, TSize1, TSize2> b_matrix;
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            a_matrix(i, j) = 2.33 * i - 4.52 * j;
 
-	b_matrix = a_matrix;
-	
-	for (std::size_t i = 0; i < a_matrix.size1(); i++)
-		for (std::size_t j = 0; j < a_matrix.size2(); j++)
-			AMATRIX_CHECK_EQUAL(b_matrix(i,j), 2.33 * i - 4.52 * j);
+    b_matrix = a_matrix;
 
-	return 0; // not failed
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            AMATRIX_CHECK_EQUAL(b_matrix(i, j), 2.33 * i - 4.52 * j);
+
+    return 0;  // not failed
 }
 
+int main() {
+    std::size_t number_of_failed_tests = 0;
+    number_of_failed_tests += TestZeroMatrixAssign<1, 1>();
 
-int main()
-{
-	std::size_t number_of_failed_tests = 0;
-	number_of_failed_tests += TestZeroMatrixAssign<1,1>();
+    number_of_failed_tests += TestZeroMatrixAssign<1, 2>();
+    number_of_failed_tests += TestZeroMatrixAssign<2, 1>();
+    number_of_failed_tests += TestZeroMatrixAssign<2, 2>();
 
-	number_of_failed_tests += TestZeroMatrixAssign<1,2>();
-	number_of_failed_tests += TestZeroMatrixAssign<2,1>();
-	number_of_failed_tests += TestZeroMatrixAssign<2,2>();
+    number_of_failed_tests += TestZeroMatrixAssign<3, 1>();
+    number_of_failed_tests += TestZeroMatrixAssign<3, 2>();
+    number_of_failed_tests += TestZeroMatrixAssign<3, 3>();
+    number_of_failed_tests += TestZeroMatrixAssign<1, 3>();
+    number_of_failed_tests += TestZeroMatrixAssign<2, 3>();
+    number_of_failed_tests += TestZeroMatrixAssign<3, 3>();
 
-	number_of_failed_tests += TestZeroMatrixAssign<3,1>();
-	number_of_failed_tests += TestZeroMatrixAssign<3,2>();
-	number_of_failed_tests += TestZeroMatrixAssign<3,3>();
-	number_of_failed_tests += TestZeroMatrixAssign<1,3>();
-	number_of_failed_tests += TestZeroMatrixAssign<2,3>();
-	number_of_failed_tests += TestZeroMatrixAssign<3,3>();
+    number_of_failed_tests += TestIdentityMatrixAssign<1>();
+    number_of_failed_tests += TestIdentityMatrixAssign<2>();
+    number_of_failed_tests += TestIdentityMatrixAssign<3>();
 
-	number_of_failed_tests += TestMatrixAssign<1,1>();
+    number_of_failed_tests += TestMatrixAssign<1, 1>();
 
-	number_of_failed_tests += TestMatrixAssign<1,2>();
-	number_of_failed_tests += TestMatrixAssign<2,1>();
-	number_of_failed_tests += TestMatrixAssign<2,2>();
+    number_of_failed_tests += TestMatrixAssign<1, 2>();
+    number_of_failed_tests += TestMatrixAssign<2, 1>();
+    number_of_failed_tests += TestMatrixAssign<2, 2>();
 
-	number_of_failed_tests += TestMatrixAssign<3,1>();
-	number_of_failed_tests += TestMatrixAssign<3,2>();
-	number_of_failed_tests += TestMatrixAssign<3,3>();
-	number_of_failed_tests += TestMatrixAssign<1,3>();
-	number_of_failed_tests += TestMatrixAssign<2,3>();
-	number_of_failed_tests += TestMatrixAssign<3,3>();
+    number_of_failed_tests += TestMatrixAssign<3, 1>();
+    number_of_failed_tests += TestMatrixAssign<3, 2>();
+    number_of_failed_tests += TestMatrixAssign<3, 3>();
+    number_of_failed_tests += TestMatrixAssign<1, 3>();
+    number_of_failed_tests += TestMatrixAssign<2, 3>();
+    number_of_failed_tests += TestMatrixAssign<3, 3>();
 
-	std::cout << number_of_failed_tests << "tests failed" << std::endl;
+    std::cout << number_of_failed_tests << "tests failed" << std::endl;
 
-	return number_of_failed_tests;
+    return number_of_failed_tests;
 }
-
