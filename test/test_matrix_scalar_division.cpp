713ec017b1305bf2a7261ed4221db247703c1dab
pooyan-dadvand <pooyan@cimne.upc.edu>
2018-04-25 12:04:44 +0200

    Adding scalar division test

--- test/test_matrix_scalar_division.cpp
+++ test/test_matrix_scalar_division.cpp
@@ -17,6 +17,22 @@ std::size_t TestMatrixScalarSelfDivision() {
     return 0;  // not failed
 }
 
+template <std::size_t TSize1, std::size_t TSize2>
+std::size_t TestMatrixScalarDivision() {
+    AMatrix::Matrix<double, TSize1, TSize2> a_matrix;
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            a_matrix(i, j) = 2.33 * i - 4.52 * j;
+
+    auto b_matrix = a_matrix / 2.3;
+    
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            AMATRIX_CHECK_EQUAL(b_matrix(i, j), (2.33 * i - 4.52 * j) / 2.3);
+
+    return 0;  // not failed
+}
+
 int main() {
     std::size_t number_of_failed_tests = 0;
 
