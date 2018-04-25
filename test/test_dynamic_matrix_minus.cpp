89832ed615ac49c11a31e67331e5e77d1667938a
pooyan-dadvand <pooyan@cimne.upc.edu>
2018-04-25 09:43:33 +0200

    Adding unary minus test

--- test/test_dynamic_matrix_minus.cpp
+++ test/test_dynamic_matrix_minus.cpp
@@ -36,6 +36,22 @@ std::size_t TestMatrixMinusEqual(std::size_t Size1, std::size_t Size2) {
     return 0;  // not failed
 }
 
+std::size_t TestMatrixUnaryMinus(std::size_t Size1, std::size_t Size2) {
+    AMatrix::Matrix<double, AMatrix::dynamic, AMatrix::dynamic> a_matrix(Size1, Size2);
+    AMatrix::Matrix<double, AMatrix::dynamic, AMatrix::dynamic> b_matrix(Size1, Size2);
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            a_matrix(i, j) = 2.33 * i - 4.52 * j;
+
+    b_matrix = -a_matrix;
+
+    for (std::size_t i = 0; i < a_matrix.size1(); i++)
+        for (std::size_t j = 0; j < a_matrix.size2(); j++)
+            AMATRIX_CHECK_EQUAL(b_matrix(i, j), -a_matrix(i,j));
+
+    return 0;  // not failed
+}
+
 int main() {
     std::size_t number_of_failed_tests = 0;
  	number_of_failed_tests += TestMatrixMinus(1,1);
@@ -63,6 +79,19 @@ int main() {
 	number_of_failed_tests += TestMatrixMinusEqual(1,3);
 	number_of_failed_tests += TestMatrixMinusEqual(2,3);
 	number_of_failed_tests += TestMatrixMinusEqual(3,3);
+
+ 	number_of_failed_tests += TestMatrixUnaryMinus(1,1);
+
+	number_of_failed_tests += TestMatrixUnaryMinus(1,2);
+	number_of_failed_tests += TestMatrixUnaryMinus(2,1);
+	number_of_failed_tests += TestMatrixUnaryMinus(2,2);
+
+	number_of_failed_tests += TestMatrixUnaryMinus(3,1);
+	number_of_failed_tests += TestMatrixUnaryMinus(3,2);
+	number_of_failed_tests += TestMatrixUnaryMinus(3,3);
+	number_of_failed_tests += TestMatrixUnaryMinus(1,3);
+	number_of_failed_tests += TestMatrixUnaryMinus(2,3);
+	number_of_failed_tests += TestMatrixUnaryMinus(3,3);
 
     std::cout << number_of_failed_tests << "tests failed" << std::endl;
 
