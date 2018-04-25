2fa699ac6083f01091a8608c19b5e50a1f5da816
pooyan-dadvand <pooyan@cimne.upc.edu>
2018-04-25 13:02:10 +0200

    Adding sub vector test

--- test/test_vector_sub_vector.cpp
+++ test/test_vector_sub_vector.cpp
@@ -22,7 +22,7 @@ std::size_t TestSubVectorAcess() {
 }
 
 template <std::size_t TSize>
-std::size_t TestSubVectorAssign() {
+std::size_t TestSubVectorMemberwiseAssign() {
     AMatrix::Vector<double, TSize> a_vector(
         AMatrix::ZeroMatrix<double>(TSize, 1));
 
@@ -43,12 +43,37 @@ std::size_t TestSubVectorAssign() {
     return 0;  // not failed
 }
 
+template <std::size_t TSize>
+std::size_t TestSubVectorAssign() {
+    AMatrix::Vector<double, TSize> a_vector(
+        AMatrix::ZeroMatrix<double>(TSize, 1));
+
+    std::size_t sub_size = (TSize > 1) ? TSize - 1 : 1;
+    std::size_t sub_index = (TSize > 1) ? 1 : 0;
+
+    AMatrix::SubVector<AMatrix::Vector<double, TSize>> sub_vector(
+        a_vector, sub_index, sub_size);
+
+    AMATRIX_CHECK_EQUAL(sub_vector.size(), sub_size);
+
+    sub_vector = AMatrix::ZeroMatrix<double>(sub_size, 1);
+
+    for (std::size_t i = sub_index; i < a_vector.size(); i++)
+        AMATRIX_CHECK_EQUAL(a_vector[i], 0);
+
+    return 0;  // not failed
+}
+
 int main() {
     std::size_t number_of_failed_tests = 0;
 
     number_of_failed_tests += TestSubVectorAcess<1>();
     number_of_failed_tests += TestSubVectorAcess<2>();
     number_of_failed_tests += TestSubVectorAcess<3>();
+
+    number_of_failed_tests += TestSubVectorMemberwiseAssign<1>();
+    number_of_failed_tests += TestSubVectorMemberwiseAssign<2>();
+    number_of_failed_tests += TestSubVectorMemberwiseAssign<3>();
 
     number_of_failed_tests += TestSubVectorAssign<1>();
     number_of_failed_tests += TestSubVectorAssign<2>();
