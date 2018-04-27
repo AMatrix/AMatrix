7bd6c3c3f7f62dcea5fc82e859dbf21cb1859212
pooyan-dadvand <pooyan@cimne.upc.edu>
2018-04-27 11:48:09 +0200

    Typo fix in check

--- test/checks.h
+++ test/checks.h
@@ -11,7 +11,7 @@
     }
 
 #define AMATRIX_CHECK_ALMOST_EQUAL(a, b)                         \
-    if (std::abs(a - b) > std::numeric_limit<double>::epsilon) { \
+    if (std::abs(a - b) > std::numeric_limits<double>::epsilon()) { \
         std::cout << a << " is not equal to " << b << std::endl; \
         return 1;                                                \
     }
