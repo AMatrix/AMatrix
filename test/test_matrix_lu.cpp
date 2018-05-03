6f9384e15b492beced457d6840f93da81a4f6fd3
pooyan-dadvand <pooyan@cimne.upc.edu>
2018-05-03 10:18:27 +0200

    Adding tests for determinant and inverse

--- test/test_matrix_lu.cpp
+++ test/test_matrix_lu.cpp
@@ -1,5 +1,6 @@
 #include "amatrix.h"
 #include "checks.h"
+#include <limits>
 
 std::size_t TestMatrixLUDeterminant3() {
     AMatrix::Matrix<double, 3, 3> a_matrix;
@@ -10,39 +11,36 @@ std::size_t TestMatrixLUDeterminant3() {
 
     a_matrix(2,2) = 0.00;
 
-    // double* A[3];
-    
-    // value = 0;
-    // for(int i = 0 ; i < 3 ; i++){
-    //     A[i] = new double[3];
-    //     for(int j = 0 ; j < 3 ; j++)
-    //         A[i][j] = value++;
-    // }
-
-    // int P[4];
-    
-    // LUPDecompose(A,3,1e-12,P);
-
-    AMatrix::Matrix<double, 3, 3> U{6,7,8,0,1,2,0,0,0};
-    AMatrix::Matrix<double, 3, 3> L{1,0,0,0,1,0,0.5,0.5,1};
-
-    AMatrix::Matrix<double, 3, 3> LU (L * U);
-
-    std::cout << LU << std::endl;
-
-    AMatrix::Matrix<double, 3, 3> result_matrix{3,4,5,
-                                               0,1,2,
-                                               0,0,-8};
-    // for(int i = 0 ; i < 3 ; i++){
-    //     for(int j = 0 ; j < 3 ; j++)
-    //         a_matrix(i,j) = A[i][j];
-    //     delete A[i];
-    // }
-
    AMatrix::LUFactorization< AMatrix::Matrix<double, 3, 3>, AMatrix::Vector<std::size_t, AMatrix::dynamic> > lu_factorization(a_matrix);
 
-    
     AMATRIX_CHECK_EQUAL(lu_factorization.Determinant(), 24);
+    
+    return 0;  // not failed
+}
+
+std::size_t TestMatrixLUInvert3() {
+    AMatrix::Matrix<double, 3, 3> a_matrix;
+    AMatrix::Matrix<double, 3, 3> a_inverse;
+    AMatrix::Matrix<double, 3, 3> correct_result{-35./24., 7./12., -1./8., 
+                                                 5./4., -1./2., 1./4., 
+                                                 -1./8., 1./4., -1./8};
+
+    double value = 0.00;
+    for(auto& i_value : a_matrix)
+        i_value = value++;
+
+    a_matrix(2,2) = 0.00;
+
+    AMatrix::LUFactorization< AMatrix::Matrix<double, 3, 3>, AMatrix::Vector<std::size_t, AMatrix::dynamic> > lu_factorization(a_matrix);
+    lu_factorization.Invert(a_inverse);
+
+    std::cout << a_inverse << std::endl;
+    std::cout << correct_result << std::endl;
+
+    
+    for(std::size_t i = 0 ; i < a_matrix.size1() ; i++)
+        for(std::size_t j = 0 ; j < a_matrix.size2() ; j++)
+            AMATRIX_CHECK_ALMOST_EQUAL(a_inverse(i,j), correct_result(i,j));
     
     return 0;  // not failed
 }
@@ -50,6 +48,7 @@ std::size_t TestMatrixLUDeterminant3() {
 int main() {
     std::size_t number_of_failed_tests = 0;
     number_of_failed_tests += TestMatrixLUDeterminant3();
+    number_of_failed_tests += TestMatrixLUInvert3();
 
     std::cout << number_of_failed_tests << "tests failed" << std::endl;
 
