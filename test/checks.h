d3f4bab4404ae39b2587d029ea6b8f1ed1b5432c
Pooyan Dadvand <pooyan@altair.com>
2018-09-27 10:56:27 +0200

    Adding line number to check macro

--- test/checks.h
+++ test/checks.h
@@ -1,17 +1,19 @@
 #define AMATRIX_CHECK(a)                               \
     if (!(a)) {                                          \
-        std::cout << #a << " is not true" << std::endl; \
+        std::cout << "in line " << __LINE__ << " : " << #a << " is not true" << std::endl; \
         return 1;                                      \
     }
 
 #define AMATRIX_CHECK_EQUAL(a, b)                                \
     if (a != b) {                                                \
-        std::cout << a << " is not equal to " << b << std::endl; \
+        std::cout << "in line " << __LINE__ << " : " << a   \
+                  << " is not equal to " << b << std::endl; \
         return 1;                                                \
     }
 
 #define AMATRIX_CHECK_ALMOST_EQUAL(a, b)                         \
     if (std::abs(a - b) > std::numeric_limits<double>::epsilon()) { \
-        std::cout << a << " is not equal to " << b << std::endl; \
+        std::cout << "in line " << __LINE__ << " : " << a           \
+                  << " is not equal to " << b << std::endl; \
         return 1;                                                \
     }
