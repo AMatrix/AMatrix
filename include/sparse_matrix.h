2f39a8b1ddcbc07e132899c1c89c10a7c74fa215
Pooyan Dadvand <pooyan@altair.com>
2019-01-24 18:58:33 +0100

    Adding matrix vector product

--- include/sparse_matrix.h
+++ include/sparse_matrix.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "sparse_matrix_storage.h" 
+#include "sparse_matrix_storage.h"
 
 namespace AMatrix {
 
-template<typename TStorageType>
+template <typename TStorageType>
 class SparseMatrix : public TStorageType {
    public:
     using base_type = TStorageType;
@@ -12,11 +12,23 @@ class SparseMatrix : public TStorageType {
     using base_type::size1;
     using base_type::size2;
 
-    SparseMatrix(std::size_t TheSize1, std::size_t TheSize2)
-        : base_type(TheSize1, TheSize2) {}
+    SparseMatrix(std::size_t TheSize1, std::size_t TheSize2) : base_type(TheSize1, TheSize2) {}
 
-    SparseMatrix(std::size_t TheSize1, std::size_t TheSize2,
-        std::size_t TheNumberOfNonZeros)
+    SparseMatrix(std::size_t TheSize1, std::size_t TheSize2, std::size_t TheNumberOfNonZeros)
         : base_type(TheSize1, TheSize2, TheNumberOfNonZeros) {}
 };
+template <typename TDataType>
+void Product(SparseMatrix<CSRMatrixStorage<TDataType>> const& A, Matrix<TDataType, dynamic, 1> const& X,
+    Matrix<TDataType, dynamic, 1>& Result) {
+    std::size_t index = 0;
+    for (std::size_t i = 0; i < A.size1(); i++) {
+        TDataType temp = TDataType();
+        std::size_t next_row_index = A.row_indices()[i + 1];
+        while (index < next_row_index) {
+            temp += A.values()[index] * X[A.column_indices()[index]];
+            index++;
+		}
+        Result[i] = temp;
+	}
 }
+}  // namespace AMatrix
