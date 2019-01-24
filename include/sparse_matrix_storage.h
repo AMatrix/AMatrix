22f829fa7207025d1f8868210f22f9bd42178520
Pooyan Dadvand <pooyan@altair.com>
2019-01-24 18:58:53 +0100

    Adding access methods to the internal storage

--- include/sparse_matrix_storage.h
+++ include/sparse_matrix_storage.h
@@ -29,7 +29,7 @@ class CSRMatrixStorage {
           _values(TheNonZerosSize),
           _zero() {
         _row_indices.fill(std::size_t());
-	}
+    }
 
     TDataType const& operator()(std::size_t i, std::size_t j) const { return at(i, j); }
 
@@ -59,10 +59,16 @@ class CSRMatrixStorage {
 
     std::size_t size() const { return _size1 * _size2; }
 
+    std::size_t non_zeros_size() const { return _non_zeros_size; }
+    Matrix<std::size_t, dynamic, 1> const& row_indices() const { return _row_indices; };
+    Matrix<std::size_t, dynamic, 1> const& column_indices() const { return _column_indices; };
+    Matrix<TDataType, dynamic, 1> const& values() const { return _values; };
+
     class non_zero_iterator {
         CSRMatrixStorage& _storage;
         std::size_t _index;
         std::size_t _row_index;
+
        public:
         using iterator_category = std::forward_iterator_tag;
         using value_type = TDataType;
@@ -70,15 +76,13 @@ class CSRMatrixStorage {
         using pointer = TDataType*;
         using reference = TDataType&;
 
-		non_zero_iterator(CSRMatrixStorage& TheStorage, std::size_t TheRowIndex, std::size_t TheIndex)
-            : _storage(TheStorage), _index(TheIndex), _row_index(TheRowIndex) {
-        }
-
+        non_zero_iterator(CSRMatrixStorage& TheStorage, std::size_t TheRowIndex, std::size_t TheIndex)
+            : _storage(TheStorage), _index(TheIndex), _row_index(TheRowIndex) {}
 
         non_zero_iterator& operator++() {
-                    _index++;
-                    return *this;
-                }
+            _index++;
+            return *this;
+        }
         non_zero_iterator operator++(int) {
             non_zero_iterator old = *this;
             ++(*this);
@@ -88,38 +92,33 @@ class CSRMatrixStorage {
         bool operator!=(non_zero_iterator other) const { return !(*this == other); }
         reference operator*() const { return _storage._values[_index]; }
 
-		std::size_t index1() {
+        std::size_t index1() {
             while ((_storage._row_indices[_row_index + 1]) <= _index && (_row_index < _storage.size1()))
                 _row_index++;
             return _row_index;
         }
         std::size_t index2() { return _storage._column_indices[_index]; }
-	
     };
 
-	non_zero_iterator begin() {
-        return non_zero_iterator(*this, 0, 0);
-    }
+    non_zero_iterator begin() { return non_zero_iterator(*this, 0, 0); }
     non_zero_iterator end() { return non_zero_iterator(*this, size1(), _non_zeros_size); }
-
 
    private:
     TDataType& insert_new_item(std::size_t I, std::size_t J, TDataType const& Value) {
-
-        const std::size_t position = _row_indices[I + 1]; // Inserting at the end of the row to move less
+        const std::size_t position = _row_indices[I + 1];  // Inserting at the end of the row to move less
 
         for (std::size_t index = I + 1; index < _size1 + 1; index++)
             _row_indices[index]++;
 
-		insert_in_position(_column_indices, position, J);
+        insert_in_position(_column_indices, position, J);
         insert_in_position(_values, position, Value);
 
-		_non_zeros_size++;
+        _non_zeros_size++;
 
         return _values[_row_indices[I + 1] - 1];
     }
 
-	template <typename TVectorType, typename TValueType>
+    template <typename TVectorType, typename TValueType>
     void insert_in_position(TVectorType& TheVector, std::size_t Position, TValueType const& Value) {
         std::size_t new_size = _row_indices[_size1];
 
@@ -134,11 +133,11 @@ class CSRMatrixStorage {
             for (std::size_t index = Position + 1; index < new_size; index++)
                 temp_vector[index] = TheVector[index - 1];
 
-			TheVector.swap(temp_vector);
+            TheVector.swap(temp_vector);
         } else {
             for (std::size_t index = new_size - 1; index >= Position; index--)
                 TheVector[index] = TheVector[index - 1];
-			
+
             TheVector[Position] = Value;
         }
     }
