f3c5495ee754c5c9f9e3b7e93de55448c77920d3
Pooyan Dadvand <pooyan@altair.com>
2019-01-28 14:00:02 +0100

    Adding different size methods and row_connectivitites method

--- include/sparse_graph.h
+++ include/sparse_graph.h
@@ -17,5 +17,20 @@ class SparseGraph {
     void insert(std::size_t I, std::size_t J) { _connectivities[I].insert(J); }
 
     std::size_t row_size(std::size_t I) const { return _connectivities[I].size(); }
+
+    std::size_t size1() const { return _size1; }
+
+    std::size_t size2() const { return _size2; }
+
+    std::size_t non_zeros_size() const {
+        std::size_t result = 0;
+
+        for (std::size_t i = 0; i < _size1; i++) {
+            result += row_size(i);
+        }
+        return result;
+    }
+
+	std::unordered_set<std::size_t> const& row_connectivities(std::size_t I) const { return _connectivities[I]; }
 };
 }  // namespace AMatrix
